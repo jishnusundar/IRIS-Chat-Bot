b3e2dded3bc7e3577806a3017d3f10527a120267
= <jishnusundar423@gmail.com>
2017-12-13 15:35:01 -0500

    First commit

---
+++ bin/run.js
@@ -0,0 +1,10 @@
+'use strict';
+const service = require('../server/service');
+const http = require('http');
+
+const server = http.createServer(service);
+server.listen(4000);
+
+server.on('listening',function () {
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode`)
+});
